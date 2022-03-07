18d4a773673194053a96eb1b63f73204d514d48d
chagy <chagy_x_@hotmail.com>
2022-03-07 13:53:37 +0700

    44

--- main.py
+++ main.py
@@ -6,14 +6,37 @@ from pydantic import BaseModel
 from typing import List,Optional
 from db import users
 from passlib.context import CryptContext
+from fastapi_login import LoginManager
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+ACCESS_TOKEN_EXPIRES_MINUTES = 60
+
+manager = LoginManager(secret=SECRET_KEY,token_url="/login",use_cookie=True)
+manager.cookie_name = "auth"
+
+@manager.user_load()
+def get_user_from_db(username:str):
+    if username in users.keys():
+        return UserDB(**users[username])
+
+def authenticate_user(username: str,password: str):
+    user = get_user_from_db(username=username)
+    if not user:
+        return None
+    if not verify_password(plain_password=password,hashed_password=user.hashed_password):
+        return None
+    return user
 
 pwd_ctx = CryptContext(schemes=["bcrypt"],deprecated="auto")
 
 def get_hashed_password(plain_password):
     return pwd_ctx.hash(plain_password) 
 
-def verify_password(plain_plassword,hashed_password):
-    return pwd_ctx.verify(plain_plassword,hashed_password)
+def verify_password(plain_password,hashed_password):
+    return pwd_ctx.verify(plain_password,hashed_password)
 
 print(get_hashed_password("password"))
 
